bb752f14053615e7c79a8bec29fdba7a88bb9b54
James Grau <backartificial@users.noreply.github.com>
2018-05-19 16:35:35 -0400

    Initial Push
    Initial setup

---
+++ James_Grau_991443203_Assignment_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace James_Grau_991443203_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
