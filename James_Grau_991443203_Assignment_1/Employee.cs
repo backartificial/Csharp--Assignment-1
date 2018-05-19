3add66ea5544ded0797b612b88a0ca1e19f7e182
James Grau <backartificial@users.noreply.github.com>
2018-05-19 17:25:34 -0400

    Initialized Employee Class
    Initialized Employee Class

---
+++ James_Grau_991443203_Assignment_1/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace James_Grau_991443203_Assignment_1 {
+    class Employee {
+        private int id;
+        private string name, profession;
+
+        public Employee(int id, string name, string profession) {
+            this.id = id;
+            this.name = name;
+            this.profession = profession;
+        }
+
+        public string employeeInformation() {
+            return "Employee ID: " + id + "\nEmployee Name: " + name + "\nEmployee Profession: " + profession;
+        }
+    }
+}
--- James_Grau_991443203_Assignment_1/Program.cs
+++ James_Grau_991443203_Assignment_1/Program.cs
@@ -6,7 +6,12 @@ namespace James_Grau_991443203_Assignment_1
     {
         static void Main(string[] args)
         {
+
+            Employee e = new Employee(1, "James Grau", "Computer Programmer");
             Console.WriteLine("Hello World!");
+            Console.WriteLine(e.employeeInformation());
+
+            Console.Read();
         }
     }
 }
