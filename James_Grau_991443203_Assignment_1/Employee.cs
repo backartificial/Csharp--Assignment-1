18b9c52702cbb9bc4ef28cba8eb252c15c5ffb55
James Grau <backartificial@users.noreply.github.com>
2018-05-21 17:52:33 -0400

    Bulk Comment
    Added mass comments and cleaned code for Employee parent and children classes (not test class yet)

--- James_Grau_991443203_Assignment_1/Employee.cs
+++ James_Grau_991443203_Assignment_1/Employee.cs
@@ -1,18 +1,59 @@
-﻿using System;
+﻿/**
+ * 
+ * File: SelfEmployee.cs
+ * Developer: James Grau
+ * Date: May 20, 2018
+ * 
+ * Assignment: 1
+ * Author: James Grau
+ * Student Number: 991443203
+ * Class: PROG 37721
+ * 
+ **/
 
+// Project Namespace
 namespace James_Grau_991443203_Assignment_1 {
+    /**
+     * 
+     * This is class is used to hold a root Employee instance
+     * 
+     **/
     abstract class Employee {
+        // Initialize the needed class variables
         protected string id, name, profession;
 
+        /**
+         * 
+         * This method is used to create a blank Employee constructor
+         * 
+         **/
         public Employee() { }
 
+        /**
+         * 
+         * This method is used to create an Employee instance with passed values
+         * 
+         * @param id: is the Employees id
+         * @param name: is the Employees name
+         * @param profession: is the Employees Profession
+         * 
+         **/
         public Employee(string id, string name, string profession) {
+            // Assign this instances' variables based on the passed values
             this.id = id;
             this.name = name;
             this.profession = profession;
         }
 
+        /**
+         * 
+         * This method is used to return employee information for the Employee instnace
+         * 
+         * @return infroamtion<string>: return the Employee information in a string
+         * 
+         **/
         public virtual string employeeInformation() {
+            // Return a string with the employees information
             return "Employee ID: " + id + "\nEmployee Name: " + name + "\nEmployee Profession: " + profession;
         }
     }
--- James_Grau_991443203_Assignment_1/SelfEmployee.cs
+++ James_Grau_991443203_Assignment_1/SelfEmployee.cs
@@ -1,13 +1,48 @@
-﻿using System;
+﻿/**
+ * 
+ * File: SelfEmployee.cs
+ * Developer: James Grau
+ * Date: May 20, 2018
+ * 
+ * Assignment: 1
+ * Author: James Grau
+ * Student Number: 991443203
+ * Class: PROG 37721
+ * 
+ **/
 
+// Project Namespace
 namespace James_Grau_991443203_Assignment_1 {
+    /**
+     * 
+     * This is class is used to hold a root SelfEmployee instance
+     * 
+     **/
     class SelfEmployee : Employee {
+        // Initialize the needed class properties with the ability to return them when needed
         private double hourlyRate { get; }
         private int totalHours { get; }
 
+        /**
+         * 
+         * This method is used to create a blank SelfEmployee constructor
+         * 
+         **/
         public SelfEmployee() : base() { }
 
+        /**
+         * 
+         * This method is used to create a SelfEmployee instance with passed values
+         * 
+         * @param id: is the Employees id
+         * @param name: is the Employees name
+         * @param profession: is the Employees Profession
+         * @param hourlyRate: is the Employees hourly rate
+         * @param totalHour: is the Employees total hours
+         * 
+         **/
         public SelfEmployee(string id, string name, string profession, double hourlyRate, int totalHours) : base(id, name, profession) {
+            // Assign this instances' variables based on the passed values
             this.id = id;
             this.name = name;
             this.profession = profession;
@@ -15,11 +50,27 @@ namespace James_Grau_991443203_Assignment_1 {
             this.totalHours = totalHours;
         }
 
+        /**
+         * 
+         * This method is used to calculate and return an Employees wage
+         * 
+         * @return wage<double>: is an Employees calculated wage
+         * 
+         **/
         public double calculateWage() {
+            // Return an Employees calculated wage based on their total hours and hourly rate
             return totalHours * hourlyRate;
         }
 
+        /**
+         * 
+         * This method is used to return the employees information (from the base class) along with the hourly wage and total hours
+         * 
+         * @return information<string>: return the Employees overall information
+         * 
+         **/
         public override string employeeInformation() {
+            // Return the base classes information along with the concatenated hourly wage and total hours worked
             return base.employeeInformation() + "\nHourly Rate: " + hourlyRate + "\nTotal Hours: " + totalHours;
         }
     }
